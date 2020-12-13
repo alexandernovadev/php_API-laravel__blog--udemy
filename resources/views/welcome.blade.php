5ed5213cbc46f3a8cb78f5a6a96ada0920ae5961
alexsk88 <titoantifa69@gmail.com>
2020-12-12 19:56:13 -0500

    UPDATED: Deploy api blog alexsk88 with migrations

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>api_blog_alexsk88</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Api_ blog</h1>
+    </body>
+</html>
